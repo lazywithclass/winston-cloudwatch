b810dd33bfdf556132b8d59f92529b5c0faee7a6
lazywithclass <montecristoh@gmail.com>
2015-01-30 13:58:18 +0000

    clarified

--- test/example.js
+++ test/example.js
@@ -1,15 +1,16 @@
 var winston = require('winston'),
-  options = {
-    logGroupName: 'testing',
-    logStreamName: 'testing',
-    awsAccessKeyId: 'your-access-key-id',
-    awsSecretKey: 'your-secret-key'
-  };
-winston.add(require('winston-cloudwatch'), options);
+    // aws keys are optional
+    options = {
+      logGroupName: 'testing',
+      logStreamName: 'testing',
+      awsAccessKeyId: 'your-access-key-id',
+      awsSecretKey: 'your-secret-key'
+    };
+winston.add(require('../index'), options);
 
-winston.silly('log this 1', { and: 'this too 1' });
-winston.debug('log this 2', { and: 'this too 2' });
-winston.verbose('log this 3', { and: 'this too 3' });
-winston.info('log this 4', { and: 'this too 4' });
-winston.warn('log this 5', { and: 'this too 5' });
-winston.error('log this 6', { and: 'this too 6' });
+winston.silly('silly', { and: 'this too 1' });
+winston.debug('debug', { and: 'this too 2' });
+winston.verbose('verbose', { and: 'this too 3' });
+winston.info('info', { and: 'this too 4' });
+winston.warn('warn', { and: 'this too 5' });
+winston.error('error', { and: 'this too 6' });
