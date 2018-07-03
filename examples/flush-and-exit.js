0aad1a0d2629470dc8f1dde71f7d4c5a32f768d4
Alberto Zaccagni <montecristoh@gmail.com>
2018-07-03 19:40:13 +0200

    Fix for example

--- examples/flush-and-exit.js
+++ examples/flush-and-exit.js
@@ -1,14 +1,17 @@
 var winston = require('winston'),
     WinstonCloudWatch = require('../index');
 
-var self = winston.add(WinstonCloudWatch, {
+var self = winston.add(new WinstonCloudWatch({
+  name: 'using-kthxbye',
   logGroupName: 'testing',
-  logStreamName: 'first'
-});
+  logStreamName: 'another',
+  awsRegion: 'us-east-1'
+}));
 
 winston.error('1');
 
 // flushes the logs and clears setInterval
-self.transports.CloudWatch.kthxbye(function() {
+var transport = self.transports.find((t) => t.name === 'using-kthxbye')
+transport.kthxbye(function() {
   console.log('bye');
 });
