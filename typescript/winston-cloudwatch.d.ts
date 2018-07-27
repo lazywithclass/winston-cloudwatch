c9b7465e1c7cf2d72f444ce2eaa5f0dfc075f670
C45tr0 <william.the.developer@gmail.com>
2018-07-26 23:22:05 -0600

    Updating typings to support winston 3

--- typescript/winston-cloudwatch.d.ts
+++ typescript/winston-cloudwatch.d.ts
@@ -1,44 +1,35 @@
-import * as winston from 'winston';
+import * as winston from "winston";
+import * as Transport from "winston-transport";
 
-import { CloudWatch, CloudWatchLogs } from 'aws-sdk';
+import { CloudWatch, CloudWatchLogs } from "aws-sdk";
 
-interface CloudWatchIntegration {
-  upload(aws: CloudWatchLogs, groupName: string, streamName: string, cb: ((err: Error, data: any) => void)): void;
-  getToken(aws: CloudWatchLogs, groupName: string, streamName: string, cb: ((err: Error, data: string) => void)): void;
-  ensureGroupPresent(aws: CloudWatchLogs, name: string, cb: ((err: Error, data: boolean) => void)): void;
-  getStream(aws: CloudWatchLogs, groupName: string, streamName: string, cb: ((err: Error, data: CloudWatchLogs.Types.DescribeLogStreamsResponse) => void)): void;
-  ignoreInProgress(cb: ((err: Error) => void)): void;
+export interface CloudwatchTransportOptions
+  extends Transport.TransportStreamOptions {
+  level?: string;
+  name?: string;
+  logGroupName?: string | (() => string);
+  logStreamName?: string | (() => string);
+  awsAccessKeyId?: string;
+  awsSecretKey?: string;
+  awsRegion?: string;
+  awsOptions?: CloudWatch.Types.ClientConfiguration;
+  jsonMessage?: boolean;
+  messageFormatter?: (logObject: winston.LogEntry) => string;
+  proxyServer?: string;
+  uploadRate?: number;
+  errorHandler?: ((err: Error) => void);
+  retentionInDays?: number;
+}
+
+export interface CloudwatchTransportInstance extends Transport {
+  new (options?: CloudwatchTransportOptions): CloudwatchTransportInstance;
+  kthxbye(cb: (() => void)): void;
 }
 
 declare module "winston" {
-  
-  export type LogObject = {level: string, msg: string, meta?: any};
-
-  export interface CloudwatchTransportOptions {
-    level?: string;
-    logGroupName?: string | (() => string);
-    logStreamName?: string | (() => string);
-    awsAccessKeyId?: string;
-    awsSecretKey?: string;
-    awsRegion?: string;
-    awsOptions?: CloudWatch.Types.ClientConfiguration;
-    jsonMessage?: boolean;
-    messageFormatter?: (logObject: LogObject) => string;
-    proxyServer?: string;
-    uploadRate?: number;
-    errorHandler?: ((err: Error) => void);
-  }
-
-  export interface Winston {
-    add(transport: winston.TransportInstance, options?: winston.TransportOptions | CloudwatchTransportOptions, created?: boolean): winston.LoggerInstance;
-  }
-
-  export interface CloudwatchTransportInstance extends winston.TransportInstance {
-    new(options?: CloudwatchTransportOptions): CloudwatchTransportInstance;
-    kthxbye(cb: (() => void)): void;
-  }
-
   export interface Transports {
-    CloudWatch: CloudwatchTransportInstance
+    CloudWatch: CloudwatchTransportInstance;
   }
 }
+
+export const WinstonCloudWatch: CloudwatchTransportInstance;
