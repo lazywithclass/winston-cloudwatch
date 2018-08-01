e189d2f507a524c20f445e22a68bbca4daf4bbb1
C45tr0 <william.the.developer@gmail.com>
2018-08-01 14:50:36 -0600

    Fixing setup after working with typings more

--- typescript/winston-cloudwatch.d.ts
+++ typescript/winston-cloudwatch.d.ts
@@ -3,33 +3,35 @@ import * as Transport from "winston-transport";
 
 import { CloudWatch, CloudWatchLogs } from "aws-sdk";
 
-export interface CloudwatchTransportOptions
-  extends Transport.TransportStreamOptions {
-  level?: string;
-  name?: string;
-  logGroupName?: string | (() => string);
-  logStreamName?: string | (() => string);
-  awsAccessKeyId?: string;
-  awsSecretKey?: string;
-  awsRegion?: string;
-  awsOptions?: CloudWatch.Types.ClientConfiguration;
-  jsonMessage?: boolean;
-  messageFormatter?: (logObject: winston.LogEntry) => string;
-  proxyServer?: string;
-  uploadRate?: number;
-  errorHandler?: ((err: Error) => void);
-  retentionInDays?: number;
+declare module WinstonCloudWatch {
+  interface CloudwatchTransportOptions
+    extends Transport.TransportStreamOptions {
+    level?: string;
+    name?: string;
+    logGroupName?: string | (() => string);
+    logStreamName?: string | (() => string);
+    awsAccessKeyId?: string;
+    awsSecretKey?: string;
+    awsRegion?: string;
+    awsOptions?: CloudWatch.Types.ClientConfiguration;
+    jsonMessage?: boolean;
+    messageFormatter?: (logObject: winston.LogEntry) => string;
+    proxyServer?: string;
+    uploadRate?: number;
+    errorHandler?: ((err: Error) => void);
+    retentionInDays?: number;
+  }
 }
 
-export interface CloudwatchTransportInstance extends Transport {
-  new (options?: CloudwatchTransportOptions): CloudwatchTransportInstance;
+declare class WinstonCloudWatch extends Transport {
+  constructor(options?: WinstonCloudWatch.CloudwatchTransportOptions);
   kthxbye(cb: (() => void)): void;
 }
 
 declare module "winston" {
   export interface Transports {
-    CloudWatch: CloudwatchTransportInstance;
+    CloudWatch: WinstonCloudWatch;
   }
 }
 
-export const WinstonCloudWatch: CloudwatchTransportInstance;
+export = WinstonCloudWatch;
