679e957533edced466a9cc4849bb58ba61333498
Manuel Spagnolo <shikaan@users.noreply.github.com>
2018-10-23 19:39:46 +0200

    [Patch] Handle circular references (#99)

--- test/utils.js
+++ test/utils.js
@@ -1,21 +1,32 @@
-describe('utils', function() {
+describe('utils', function () {
 
-  describe('stringify', function() {
+  describe('stringify', function () {
 
     const lib = require('../lib/utils')
 
-    it('stringifies an object', function() {
+    it('stringifies an object', function () {
       const object = { answer: 42 }
       lib.stringify(object).should.equal(JSON.stringify(object, null, '  '))
     })
 
-    it('stringifies an Error istance', function() {
+    it('stringifies an Error istance', function () {
       const error = new Error('uh-oh'),
-            result = JSON.parse(lib.stringify(error))
+        result = JSON.parse(lib.stringify(error))
       result.message.should.equal('uh-oh')
       result.stack.should.be.an.instanceOf(String)
     });
 
+    it('handles circular objects', function () {
+      const circular = {}
+      const child = { circular }
+      circular.child = child
+
+      const stringify = () => {
+        lib.stringify(circular)
+      }
+
+      stringify.should.not.throw()
+    })
   })
 
 })
