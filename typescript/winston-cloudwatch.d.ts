1250eb90398a2210fad43e8b7aa544a2f73a3c37
Kim Persson <kimpersson88@gmail.com>
2019-01-14 11:39:06 +0100

    ADDS updated typescript definitions

--- typescript/winston-cloudwatch.d.ts
+++ typescript/winston-cloudwatch.d.ts
@@ -1,18 +1,45 @@
-import * as winston from 'winston';
+import * as winston from "winston";
+import * as Transport from "winston-transport";
 
-import { CloudWatch, CloudWatchLogs } from 'aws-sdk';
+import { CloudWatch, CloudWatchLogs } from "aws-sdk";
 
-interface CloudWatchIntegration {
-  upload(aws: CloudWatchLogs, groupName: string, streamName: string, cb: ((err: Error, data: any) => void)): void;
-  getToken(aws: CloudWatchLogs, groupName: string, streamName: string, cb: ((err: Error, data: string) => void)): void;
-  ensureGroupPresent(aws: CloudWatchLogs, name: string, cb: ((err: Error, data: boolean) => void)): void;
-  getStream(aws: CloudWatchLogs, groupName: string, streamName: string, cb: ((err: Error, data: CloudWatchLogs.Types.DescribeLogStreamsResponse) => void)): void;
-  ignoreInProgress(cb: ((err: Error) => void)): void;
-}
+export namespace WinstonCloudWatch {
+  export type LogObject = { level: string; msg: string; meta?: any };
 
-declare module "winston" {
-  
-  export type LogObject = {level: string, msg: string, meta?: any};
+  export interface CloudWatchIntegration {
+    upload(
+      aws: CloudWatchLogs,
+      groupName: string,
+      streamName: string,
+      logEvents: any[],
+      retentionInDays: number,
+      cb: ((err: Error, data: any) => void)
+    ): void;
+    getToken(
+      aws: CloudWatchLogs,
+      groupName: string,
+      streamName: string,
+      retentionInDays: number,
+      cb: ((err: Error, data: string) => void)
+    ): void;
+    ensureGroupPresent(
+      aws: CloudWatchLogs,
+      name: string,
+      retentionInDays: number,
+      cb: ((err: Error, data: boolean) => void)
+    ): void;
+    getStream(
+      aws: CloudWatchLogs,
+      groupName: string,
+      streamName: string,
+      cb: ((
+        err: Error,
+        data: CloudWatchLogs.Types.DescribeLogStreamsResponse
+      ) => void)
+    ): void;
+    ignoreInProgress(cb: ((err: Error) => void)): void;
+    new (options?: CloudwatchTransportOptions): winston.Logger;
+  }
 
   export interface CloudwatchTransportOptions {
     level?: string;
@@ -30,16 +57,12 @@ declare module "winston" {
     silent?: boolean;
   }
 
-  export interface Winston {
-    add(transport: winston.TransportInstance, options?: winston.TransportOptions | CloudwatchTransportOptions, created?: boolean): winston.LoggerInstance;
-  }
-
-  export interface CloudwatchTransportInstance extends winston.TransportInstance {
-    new(options?: CloudwatchTransportOptions): CloudwatchTransportInstance;
+  export interface CloudwatchTransportInstance extends winston.Logger {
+    new (options?: CloudwatchTransportOptions): CloudwatchTransportInstance;
     kthxbye(cb: (() => void)): void;
   }
 
   export interface Transports {
-    CloudWatch: CloudwatchTransportInstance
+    CloudWatch: CloudwatchTransportInstance;
   }
 }
